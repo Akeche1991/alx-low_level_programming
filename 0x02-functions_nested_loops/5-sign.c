2a8e4119880137436359b4e364cfb608680cd5fa
Robert Akeche <akecherobert@gmail.com>
2023-02-21 07:44:44 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,8 +2,8 @@
 
 /**
  * print_sign - Prints the sign of a number
- * @c: The number to be checked
- * Return 1 for positive num, -1 for negative num or 0 for anything else
+ * @n: The number to be checked
+ * Return 1 for positive num, -1 for negative num or zero for anything else
  */
 
 int print_sign(int n)
@@ -23,5 +23,4 @@ int print_sign(int n)
 	_putchar(48);
 	return (0);
 	}
-	return (n);
 }
