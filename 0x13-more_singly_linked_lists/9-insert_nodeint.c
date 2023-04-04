a15aec1f39cf4dad15b9ca999d91fa43fc35d347
Robert Akeche <akecherobert@gmail.com>
2023-04-04 03:34:35 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,44 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
+
 /**
- * insert_nodeint_at_index - inserts node at given index, preserving list
- * @head: head of list
- * @idx: index to put node at
- * @n: integer data for new node
- *
- * Return: pointer to new node at specified index, NULL if not found
- */
+  * insert_nodeint_at_index - ...
+  * @head: ...
+  * @idx: ...
+  * @n: ...
+  *
+  * Return: ...
+  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int ctr = 0;
-	listint_t *temp_h, *new_node;
+	listint_t *new_node, *iter;
+	unsigned int count = 1;
 
-	if (!head || !*head) /* EMPTY list */
-		return (NULL);
-	temp_h = *head;
-	if (idx == 0) /* insert as first element */
+	if (head)
 	{
 		new_node = malloc(sizeof(listint_t));
-		if (!new_node)
+		if (new_node == NULL)
 			return (NULL);
-		new_node->n = n; /* set data */
-		new_node->next = temp_h;
-		*head = new_node;
+
+		new_node->n = n;
+		if (idx > 0)
+		{
+			iter = *head;
+			while (iter)
+			{
+				if (count == idx)
+				{
+					new_node->next = iter->next;
+					iter->next = new_node;
+					return (new_node);
+				}
+				iter = iter->next;
+				count++;
+			}
+			if (idx > count)
+				return (NULL);
+		}
+		else
+		{
+			new_node->next = *head;
+			*head = new_node;
+		}
 		return (new_node);
 	}
-	while (temp_h)
-	{
-		if (ctr + 1 == idx) /* insert node here */
-		{
-			new_node = malloc(sizeof(listint_t));
-			if (!new_node)
-				return (NULL);
-			new_node->n = n; /* set data */
-			new_node->next = temp_h->next;
-			temp_h->next = new_node;
-			return (new_node);
-		}
-		temp_h = temp_h->next;
-		ctr++;
-	}
-	return (NULL); /* didn't find node */
+	return (NULL);
 }
